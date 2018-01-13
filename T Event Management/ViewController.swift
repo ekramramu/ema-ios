7f3842ecec763b34da7f047749352693e1647a0b
Zahidur Rahman Faisal <zr.faisal@programmer.net>
2018-01-13 23:51:06 +0800

    Initial Commit

---
+++ T Event Management/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  T Event Management
+//
+//  Created by Zahidur Rahman Faisal on 13/1/18.
+//  Copyright © 2018 Zahidur Rahman Faisal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
