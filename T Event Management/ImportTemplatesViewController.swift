d9954d7e2ca837e06ac18d6f831271811ce2a009
Zahidur Rahman Faisal <zr.faisal@programmer.net>
2018-01-16 22:57:29 +0800

    Implemented ImprotTemplateViewController UI

---
+++ T Event Management/ImportTemplatesViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ImportTemplatesViewController.swift
+//  T Event Management
+//
+//  Created by Zahidur Rahman Faisal on 16/1/18.
+//  Copyright © 2018 Zahidur Rahman Faisal. All rights reserved.
+//
+
+import UIKit
+
+class ImportTemplatesViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    static func instantiate() -> ImportTemplatesViewController {
+        return UIStoryboard(name: "Main", bundle: nil)
+            .instantiateViewController(withIdentifier: "ImportTemplatesViewController") as! ImportTemplatesViewController
+    }
+    
+}
+
+extension ImportTemplatesViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TemplateCell", for: indexPath) as! TemplateCell
+        
+        cell.imageView.image = UIImage.init(named: "sample_cagetory.png")
+        cell.layer.cornerRadius = 4
+        
+        return cell
+    }
+    
+}
+
+extension ImportTemplatesViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    }
+    
+}
---
+++ T Event Management/TemplateCell.swift
@@ -0,0 +1,20 @@
+//
+//  TemplateCell.swift
+//  T Event Management
+//
+//  Created by Zahidur Rahman Faisal on 16/1/18.
+//  Copyright © 2018 Zahidur Rahman Faisal. All rights reserved.
+//
+
+import UIKit
+
+class TemplateCell: UICollectionViewCell {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
